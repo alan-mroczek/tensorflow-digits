2e23e0cc425e255bfd8da4f227c19867fda0cbdd
Alan Mroczek <alan.mroczek@gmail.com>
2023-01-10 02:53:37 +0100

    cleanup main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ mnist = tf.keras.datasets.mnist
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10)
 ])
 
 predictions = model(x_train[:1]).numpy()
@@ -19,20 +19,14 @@ tf.nn.softmax(predictions).numpy()
 
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-loss_fn(y_train[:1], predictions).numpy()
-
 model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
 
-log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(
+    log_dir=log_dir, histogram_freq=1)
 
 model.fit(x_train, y_train, epochs=15, callbacks=[tensorboard_callback])
 
 model.evaluate(x_test,  y_test, verbose=2)
-
-probability_model = tf.keras.Sequential([
-  model,
-  tf.keras.layers.Softmax()
-])
